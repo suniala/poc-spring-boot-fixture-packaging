83097782a473e365ad137d281464247c13c5dd5a
Mikko Suniala <mikko.suniala@iki.fi>
2023-03-18 17:00:10 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/fixturepackaging/FixturePackagingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.fixturepackaging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FixturePackagingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
